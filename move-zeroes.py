12ca8fe7a46e26c184306d7d2064fc5e21d954d5
mnpenchev <mnpenchev@gmail.com>
2020-11-03 13:13:36 +0000

    .

--- move-zeroes.py
+++ move-zeroes.py
@@ -2,23 +2,37 @@
 by moving all the number to the end and track their count
 Time complexity = O(2*) loop 2 times trough the list = O(N)
 space complexity = O(1) """
-from typing import List
 
 
-class Solution:
-    def moveZeroes(self, nums : List[int]):
-        j = 0
-        for num in nums:
-            if num != 0:
-                nums[j] = num
-                j += 1
+def moveZeroes(nums):
+    j = 0
+    for num in nums:
+        if num != 0:
+            nums[j] = num
+            j += 1
 
-        for x in range(j, len(nums)):
-            nums[x] = 0
-        print(nums)
-
-
-s = Solution()
-s.moveZeroes([0, 3, 0, 5, 4, 0])
+    for x in range(j, len(nums)):
+        nums[x] = 0
+    return nums
 
 
+print(moveZeroes([None, 0, False, 0, 3, "a", 5, 4, 0]))
+
+
+def move_zeros_alltypesdata(array):
+    new = []
+    zeros = []
+
+    for i in range(len(array)):
+        if array[i] == 0 or array[i] == 0.0:
+            if type(array[i]) == int or type(array[i]) == float:
+                zeros.append(array[i])
+            else:
+                new.append(array[i])
+        else:
+            new.append(array[i])
+
+    return new + zeros
+
+
+print(move_zeros_alltypesdata([None, 0, False, 0, 3, "a", 5, 4, 0]))
